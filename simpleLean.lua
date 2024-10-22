c8c1e7441a08b750db97cdc085fb9ff865bf06d2
XanderCreates <69139978+XanderCreates@users.noreply.github.com>
2024-10-22 06:13:35 -0400

    --tiny cleanup

--- simpleLean.lua
+++ simpleLean.lua
@@ -58,7 +58,6 @@ local part = cfg.parts
 local control = cfg.control
 ---[[Init Vars]]---
 local lean, breathe, vHead, lHead = vec3(0,0,0)
---local headRotation = part.head:getRot()
 local leanIntensity = 0
 
 ---[[Script]]---
